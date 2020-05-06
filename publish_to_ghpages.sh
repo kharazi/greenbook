1a62bb6cbba2c1e49b248cb5804c938f31616294
kharazi <vahid@kharazi.net>
2020-05-06 20:41:47 +0430

    Add push to publish script

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -24,6 +24,6 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
 
