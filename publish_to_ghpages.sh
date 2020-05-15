0406af1c8f81330da9515ebade1e993b841a18e1
kharazi <vahid@kharazi.net>
2020-05-16 00:44:42 +0430

    Automatticaly push CNAME

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -21,6 +21,9 @@ rm -rf public/*
 echo "Generating site"
 hugo --minify
 
+echo "Copy CNAME file"
+cp CNAME public/CNAME
+
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
